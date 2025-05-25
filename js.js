43669ea624f499cf86756df11a8fb004f3d667dc
Dhiren <dhirendrayogi00@gmail.com>
2025-05-25 07:31:55 +0545

    js api garako

--- js.js
+++ js.js
@@ -1,1 +1,36 @@
-console.log("helo")
+const data=[
+    {id:1,name:"sefina",age:25,gender:"female",hasPassout:true, occupation:undefined},
+    {id:2,name:"Jalan",age:25,gender:"male",hasPassout:false, occupation:undefined},
+    {id:3,name:"nirpa",age:25,gender:"male",hasPassout:true, occupation:"intern"},
+    {id:4,name:"gaurav",age:23,gender:"male",hasPassout:false, occupation:"animator"},
+    {id:5,name:"ishowr",age:100000,gender:"undefined",hasPassout:true, occupation:"be god"},
+    {id:6,name:"sefina",age:25,gender:"female",hasPassout:true, occupation:undefined},
+    {id:7,name:"bimal",age:23,gender:"male",hasPassout:true, occupation:"SSME"}
+
+    
+        
+    
+]
+
+const response={
+    status:200,
+    message:"data fetched sucessfully",
+    data:data
+}
+
+const getUserName=()=>{
+    const APIStatus=response.status
+    if(APIStatus===200){
+        const result=response.data.map((user)=>user.name)
+         return result 
+
+    }
+    else{
+
+    }
+   
+
+}
+
+
+console.log(getUserName());
